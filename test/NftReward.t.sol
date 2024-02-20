cd9a90caa3ed266d6b3f1cf2b65b467ee1d2846a
gpylypchuk <geronimopylypchuk@gmail.com>
2024-02-20 13:45:34 -0300

    add: foundry specific solidity verison

--- test/NftReward.t.sol
+++ test/NftReward.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {NftReward} from "../src/NftReward.sol";
